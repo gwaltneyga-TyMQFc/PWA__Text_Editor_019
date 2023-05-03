a90e2d7810b9b517cf163b049d79a78698401062
Gregory A. Gwaltney <gwaltneyga@gmail.com>
2023-05-03 11:06:13 -0400

    Updating Database connections 2

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log ('PUT to the database')
+  //Add create the database with a version number of 1
+  const jateDb = await openDB('jate',1);
+  //Call the process of storing the data by readrwriting since we are putting data in
+  const tx = jateDb.transaction('jate', 'readwrite');
+  //Ensure the data has an object to be stored in using the const to store it.
+  const store = tx.objectStore('jate');
+  //Request the data to be stored in the object "store"
+  const request = store.put({id:1, value:content});
+  //Ensure it waits to check the result of storing before continuing.
+  const conclusion = await request;
+  console.log("Data Saved to the database", conclusion.value);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+}
+export const getDb = async (content) => {
+  console.log ('GET from the database')
+  //Add create the database with a version number of 1
+  const jateDb = await openDB('jate',1);
+  //Call the process of getting the data by readonly since we are pulling for display.
+  const tx = jateDb.transaction('jate', 'readonly');
+  //Ensure the data has an object to be stored in using the const to store it.
+  const store = tx.objectStore('jate');
+  //Request the data to be gathered in the object "store"
+  const request = store.get(1);
+  //Ensure it waits to check the result of getting it before continuing.
+  const conclusion = await request;
+  conclusion
+    ? console.log("Data retrieved!", result.value)
+    : console.log("Data not found.");
+    
+  return result?.value;
+
+}
 
 initdb();
